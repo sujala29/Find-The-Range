0143ef5e71942614dc27e8923fc59a3a27a5ebe5
sujala Potula <sujalapotula1@gmail.com>
2022-11-09 12:25:58 -0600

    first commit

---
+++ findTheRange.js
@@ -0,0 +1,9 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+
+let min = Math.min(...array)
+let max = Math.max(...array)
+console.log("Smaller number is",min);
+console.log("Larger number is",max);
